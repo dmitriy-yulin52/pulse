3b1d6f7174856e53fc6a3e90ab567d659b6195e5
di <yulin_dimon@mail.ru>
2021-03-16 22:49:42 +0300

    validate form

--- js/script.js
+++ js/script.js
@@ -43,27 +43,64 @@ $(document).ready(function() {
         })
     });
 
-    $('#consultation-form').validate();
-    $('#consultation form').validate({
-        rules: {
-            name: "required",
-            phone: "required",
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: "Пожалуйста, введите свое имя",
-            phone: "Пожалуйста, введите свой номер телефона",
-            email: {
-                required: "Пожалуйста, введите свою почту",
-                email: "Неправильно введен адрес почты"
-            }
-        }
-    });
-    $('#order form').validate();
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Пожалуйста, введите свое имя",
+    //             minlength: jQuery.validator.format("Введите {0} символа!")
+    //         },
+    //         phone: "Пожалуйста, введите свой номер телефона",
+    //         email: {
+    //             required: "Пожалуйста, введите свою почту",
+    //             email: "Неправильно введен адрес почты"
+    //         }
+    //     }
+    // });
+    // $('#order form').validate();
 
 
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
 });
