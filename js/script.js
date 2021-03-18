a2d1faf384e7c36a11383114d17e1d1ec9f12570
di <yulin_dimon@mail.ru>
2021-03-18 23:14:19 +0300

    local server php

--- js/script.js
+++ js/script.js
@@ -103,4 +103,24 @@ $(document).ready(function() {
     validateForms('#consultation form');
     validateForms('#order form');
 
+
+    $('input[name=phone').mask("+7 (999) 999-99-99");
+
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
 });
