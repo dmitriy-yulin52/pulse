a2f170e5e466d4d30ba370e1eebf0fd5029a50ec
di <yulin_dimon@mail.ru>
2021-03-19 20:49:21 +0300

    game over

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$().ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
@@ -122,5 +122,24 @@ $(document).ready(function() {
         });
         return false;
     });
+
+
+
+    // scroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+    new WOW().init();
+
 
 });
